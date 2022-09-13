59ec7fb785b5b1b70ea6cd4e770e7ecee7831321
tamanna <tamanna202163@gmail.com>
2022-09-13 15:30:35 +0600

    dropdown added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let click =document.querySelector('.dropdown-click');
+let list =document.querySelector('.dropdown-list');
+click.addEventListener("click", () =>{
+list.classList.toggle('newListDropdown')
+});
+
+
+// 2nd
+let click1 =document.querySelector('.dropdown-click-rock');
+let list1 =document.querySelector('.dropdown-list-rock');
+click1.addEventListener("click", () =>{
+list1.classList.toggle('newListDropdown')
+});
